fff1753aa0fa2fce82b013a821a8312f2d3cb17f
kedaranatha <kedaranathakv22@gmail.com>
2024-08-27 23:58:39 +0530

    Break statements using java

---
+++ Tea.java
@@ -0,0 +1,21 @@
+class Tea{
+	public static void main(String[] args){
+	int n=2;
+	switch(n){
+	case 1:
+		System.out.println("order plz");
+		break;
+	case 2:
+		System.out.println("Tea preparing");
+		break;
+	case 3: 
+		System.out.println("tea is ready");
+		break;
+	case 4:
+	    System.out.println("Plz pay bill");
+	    break;
+	default:
+		System.out.println("Wrong order");
+		}
+	}
+}
